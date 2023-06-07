86e3e71f662373388a6199787e8a9a13c4345754
Sarwar <2plus7sarwar@gmail.com>
2023-06-07 09:50:08 +0600

    added intro section

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeBanner from './HomeBanner';
 import OrderOnline from './OrderOnline';
+import Intro from './Intro';
 
 const Homepage = () => {
     return (
         <div>
             <HomeBanner></HomeBanner>
             <OrderOnline></OrderOnline>
+            <Intro></Intro>
         </div>
     );
 };
---
+++ src/pages/Home/Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import cover from '../../assets/home/chef-service.jpg'
+
+const Intro = () => {
+    return (
+        <div className='mx-10 relative mb-[100px]'>
+            <img className='w-full object-cover ' src={cover} alt="" />
+            <div className='bg-dark/40 p-20 w-8/12 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <h2 className='text-2xl uppercase mb-5 text-light font-semibold'>Food villa</h2>
+                <p className='text-sm text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, dolores? Dolor deleniti ex labore at beatae voluptatum architecto laudantium sed, modi earum illo qui dolorem eveniet possimus! Quae, pariatur nostrum.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
